95d87a502b942a86d4bede7644bb63604f61252d
Connor Ekins <ekins.connor@gmail.com>
2024-09-10 13:09:08 -0600

    Added some styling

--- assignmentsFolder/02-counterApp/index.js
+++ assignmentsFolder/02-counterApp/index.js
@@ -2,17 +2,19 @@ let count = 0;
 
 function increment() {
     count += 1;
-    document.getElementById("countEl").innerText = count;
+    countEl.innerText = count;
 }
 
 function decrement() {
-    count -= 1;
-    document.getElementById("countEl").innerText = count;
+    if (count > 0) {
+        count -= 1;
+        countEl.innerText = count;
+    }
 }
 
 function reset() {
     count = 0;
-    document.getElementById("countEl").innerText = count;
+    countEl.innerText = count;
 }
 
 // function save() {}
