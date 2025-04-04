658c04d8a941cf152ddc7977523302e29b350311
Connor Ekins <ekins.connor@gmail.com>
2025-04-03 20:53:32 -0600

    Started the JS for the site.

--- assignmentsFolder/06-wellnessTracker/js/scripts.js
+++ assignmentsFolder/06-wellnessTracker/js/scripts.js
@@ -1,1 +1,8 @@
 console.log("I am connected");
+
+const saveBtn = document.querySelector('#saveProfile');
+
+saveBtn.addEventListener('click', () => {
+    let fn = document.querySelector('input[name="first"]');
+    console.log(fn);
+})
