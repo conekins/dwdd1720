6b40d743bec99101a7f67c1a6ced206ee1eadcc9
Connor Ekins <ekins.connor@gmail.com>
2025-04-06 17:33:16 -0600

    Added more JS

--- assignmentsFolder/06-wellnessTracker/js/scripts.js
+++ assignmentsFolder/06-wellnessTracker/js/scripts.js
@@ -3,7 +3,11 @@ console.log("I am connected");
 const saveBtn = document.querySelector('#saveBtn');
 
 saveBtn.addEventListener('click', () => {
-    event.preventDefault();
     let fn = document.querySelector('input[name="first"]');
-    console.log(fn.value);
-})
+    let ln = document.querySelector('input[name="last"]');
+    let emotionSelect = document.querySelector('select[name="emotionalState"]');
+
+    localStorage.setItem('wellness-first', fn.value);
+    localStorage.setItem('wellness-last', ln.value);
+    localStorage.setItem('wellness-emotion', emotionSelect.value);
+});
