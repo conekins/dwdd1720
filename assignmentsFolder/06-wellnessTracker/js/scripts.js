e09dddb3f67b88d8e6f0a6d354f3cd33f833147e
Connor Ekins <ekins.connor@gmail.com>
2025-04-10 12:43:43 -0600

    Added more to the JS.

--- assignmentsFolder/06-wellnessTracker/js/scripts.js
+++ assignmentsFolder/06-wellnessTracker/js/scripts.js
@@ -1,19 +1,31 @@
 console.log("I am connected");
 
 const saveBtn = document.querySelector('#saveBtn');
+const deleteBtn = document.querySelector('#deleteBtn');
 
 saveBtn.addEventListener('click', () => {
     let fn = document.querySelector('input[name="first"]');
     let ln = document.querySelector('input[name="last"]');
-    let emotionSelect = document.querySelector('select[name="emotionalState"]');
+    let hrs = document.querySelector('input[name="hours"]');
+    let min = document.querySelector('input[name="minutes"]');
+    let exerciseSelect = document.querySelector('select[name="exerciseType"]');
+    let selectedOption = exerciseSelect.options[exerciseSelect.selectedIndex];
 
-    localStorage.setItem('wellness-first', fn.value);
-    localStorage.setItem('wellness-last', ln.value);
-    localStorage.setItem('wellness-emotion', emotionSelect.value);
+    let exerciseName = selectedOption.value;
+    let exerciseImage = selectedOption.dataset.url;
+
+    localStorage.setItem('exercise-first', fn.value);
+    localStorage.setItem('exercise-last', ln.value);
+    localStorage.setItem('exercise-hr', hrs.value);
+    localStorage.setItem('exercise-min', min.value);
 });
 
-document.querySelector('#first').textContent = localStorage.getItem('wellness-first');
+deleteBtn.addEventListener('click', () => {
+    localStorage.clear();
+})
 
-document.querySelector('#last').textContent = localStorage.getItem('wellness-last');
-
-document.querySelector('#emotionalState').textContent = localStorage.getItem('wellness-emotion');
+document.querySelector('#first').textContent = localStorage.getItem('exercise-first');
+document.querySelector('#last').textContent = localStorage.getItem('exercise-last');
+// Hours and minutes are being stored in localStorage but they are not being pulled and displayed.
+document.querySelector('#hours').textContent = localStorage.getItem('exercise-hr');
+document.querySelector('#minutes').textContent = localStorage.getItem('exercise-min');
