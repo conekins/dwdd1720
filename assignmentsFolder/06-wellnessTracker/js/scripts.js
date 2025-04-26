9d8d60a70a4d93bc50ab011cb80832499fd731d8
Connor Ekins <ekins.connor@gmail.com>
2025-04-26 16:18:21 -0600

    Made some updates to the JS

--- assignmentsFolder/06-wellnessTracker/js/scripts.js
+++ assignmentsFolder/06-wellnessTracker/js/scripts.js
@@ -3,26 +3,26 @@ console.log("I am connected");
 const saveBtn = document.querySelector('#saveBtn');
 const deleteBtn = document.querySelector('#deleteBtn');
 
-saveBtn.addEventListener('click', () => {
+saveBtn.addEventListener('click', (event) => {
     event.preventDefault();
 
     let fn = document.querySelector('input[name="first"]');
     let ln = document.querySelector('input[name="last"]');
     let hrs = document.querySelector('input[name="hours"]');
     let min = document.querySelector('input[name="minutes"]');
-    let exerciseSelect = document.querySelector('select[name="exerciseType"]');
-    let selectedOption = exerciseSelect.options[exerciseSelect.selectedIndex];
-
-    let exerciseName = selectedOption.value;
-    let exerciseImage = selectedOption.dataset.url;
+    let exercise = document.querySelector('select[name="exerciseType"]');
+    
 
     localStorage.setItem('exercise-first', fn.value);
     localStorage.setItem('exercise-last', ln.value);
+    localStorage.setItem('exercise-type', exercise.value);
     localStorage.setItem('exercise-hr', hrs.value);
     localStorage.setItem('exercise-min', min.value);
 
     document.querySelector('#first').textContent = localStorage.getItem('exercise-first');
     document.querySelector('#last').textContent = localStorage.getItem('exercise-last');
+    document.querySelector('#displayExercise').textContent = localStorage.getItem('exercise-type');
+
     document.querySelector('#displayHours').textContent = localStorage.getItem('exercise-hr');
     document.querySelector('#displayMinutes').textContent = localStorage.getItem('exercise-min');
 });
@@ -32,6 +32,7 @@ deleteBtn.addEventListener('click', () => {
     localStorage.clear();
     document.querySelector('#first').textContent = 'John';
     document.querySelector('#last').textContent = 'Doe';
-    document.querySelector('#hours').textContent = 'X';
-    document.querySelector('#minutes').textContent = 'X';
+    document.querySelector('#displayExercise').textContent = 'example exercise';
+    document.querySelector('#displayHours').textContent = 'X';
+    document.querySelector('#displayMinutes').textContent = 'X';
 });
