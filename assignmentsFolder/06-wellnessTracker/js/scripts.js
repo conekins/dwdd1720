97c9bbef56044fd17ed8ffff710e04338ad9a497
Connor Ekins <50034278+conekins@users.noreply.github.com>
2025-09-23 02:10:03 -0600

    Honestly I don't even know on this one, but it all works.

--- assignmentsFolder/06-wellnessTracker/js/scripts.js
+++ assignmentsFolder/06-wellnessTracker/js/scripts.js
@@ -11,28 +11,44 @@ saveBtn.addEventListener('click', (event) => {
     let hrs = document.querySelector('input[name="hours"]');
     let min = document.querySelector('input[name="minutes"]');
     let exercise = document.querySelector('select[name="exerciseType"]');
-    
+    let exerciseImg = document.querySelector('#exerciseType option:checked');
 
     localStorage.setItem('exercise-first', fn.value);
     localStorage.setItem('exercise-last', ln.value);
     localStorage.setItem('exercise-type', exercise.value);
+    localStorage.setItem('exercise-img', exerciseImg.dataset.url);
     localStorage.setItem('exercise-hr', hrs.value);
     localStorage.setItem('exercise-min', min.value);
+    location.reload();
+});
 
+    const check = localStorage.getItem('exercise-first');
+    console.log(check);
+
+    if (check === null) {
+    document.querySelector('#newProfile').className = 'showMe';
+    document.querySelector('#myProfile').className = 'hideMe';
+} else {
     document.querySelector('#first').textContent = localStorage.getItem('exercise-first');
     document.querySelector('#last').textContent = localStorage.getItem('exercise-last');
     document.querySelector('#displayExercise').textContent = localStorage.getItem('exercise-type');
+    document.querySelector('#emoji').src = localStorage.getItem('exercise-img');
 
     document.querySelector('#displayHours').textContent = localStorage.getItem('exercise-hr');
     document.querySelector('#displayMinutes').textContent = localStorage.getItem('exercise-min');
-});
+
+    document.querySelector('#newProfile').className = 'hideMe';
+    document.querySelector('#myProfile').className = 'showMe';
+}
+
 
 deleteBtn.addEventListener('click', () => {
     // Clear localStorage and reset the profile display
-    localStorage.clear();
-    document.querySelector('#first').textContent = 'John';
-    document.querySelector('#last').textContent = 'Doe';
-    document.querySelector('#displayExercise').textContent = 'example exercise';
-    document.querySelector('#displayHours').textContent = 'X';
-    document.querySelector('#displayMinutes').textContent = 'X';
+    localStorage.removeItem('exercise-first');
+    localStorage.removeItem('exercise-last');
+    localStorage.removeItem('exercise-type');
+    localStorage.removeItem('exercise-img');
+    localStorage.removeItem('exercise-hr');
+    localStorage.removeItem('exercise-min');
+    location.reload();
 });
