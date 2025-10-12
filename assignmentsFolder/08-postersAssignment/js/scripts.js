7821bbd24226034be5cd3f281204b50424492aa8
Connor Ekins <50034278+conekins@users.noreply.github.com>
2025-10-11 18:25:43 -0600

    Changed some CSS and added a footer

--- assignmentsFolder/08-postersAssignment/js/scripts.js
+++ assignmentsFolder/08-postersAssignment/js/scripts.js
@@ -1,1 +1,7 @@
-console.log("I am connected");
+const btnRed = document.querySelector("#redImage")
+const btnGreen = document.querySelector("#greenImage")
+
+const pageBody = document.querySelector("div")
+
+btnRed.addEventListener('click', () => pageBody.className = "paintRed")
+btnGreen.addEventListener('click', () => pageBody.className = "paintGreen")
