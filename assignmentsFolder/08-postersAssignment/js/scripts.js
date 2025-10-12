190c110bd98e3948827326468254b724bf57a2e3
Connor Ekins <50034278+conekins@users.noreply.github.com>
2025-10-11 19:44:53 -0600

    Set up the base to convert into the assignment pt.2

--- assignmentsFolder/08-postersAssignment/js/scripts.js
+++ assignmentsFolder/08-postersAssignment/js/scripts.js
@@ -1,7 +1,7 @@
 const btnRed = document.querySelector("#redImage")
 const btnGreen = document.querySelector("#greenImage")
 
-const pageBody = document.querySelector("div")
+const pageBody = document.querySelector("body")
 
 btnRed.addEventListener('click', () => pageBody.className = "paintRed")
 btnGreen.addEventListener('click', () => pageBody.className = "paintGreen")
