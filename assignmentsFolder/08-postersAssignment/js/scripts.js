4dc838a9201a4e83e09295bbe6f2741be51b98a7
Connor Ekins <50034278+conekins@users.noreply.github.com>
2025-10-11 23:58:56 -0600

    Started to convert from the example to the assignment requirements

--- assignmentsFolder/08-postersAssignment/js/scripts.js
+++ assignmentsFolder/08-postersAssignment/js/scripts.js
@@ -1,7 +1,15 @@
-const btnRed = document.querySelector("#redImage")
-const btnGreen = document.querySelector("#greenImage")
+const gridBtn = document.querySelector("#gridIcon");
+const listBtn = document.querySelector("#listIcon");
 
-const pageBody = document.querySelector("body")
+const pageBody = document.querySelector("body");
 
-btnRed.addEventListener('click', () => pageBody.className = "paintRed")
-btnGreen.addEventListener('click', () => pageBody.className = "paintGreen")
+gridBtn.addEventListener('click', () => {
+    pageBody.className = "grid";
+    gridBtn.classList.add("is-hidden");
+    listBtn.classList.remove("is-hidden");
+});
+listBtn.addEventListener('click', () => {
+    pageBody.className = "list";
+    listBtn.classList.add("is-hidden");
+    gridBtn.classList.remove("is-hidden");
+});
