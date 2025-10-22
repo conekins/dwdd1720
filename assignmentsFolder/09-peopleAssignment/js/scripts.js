1e9abb3aff0e7d973ed13e47d8a78988f54ec544
Connor Ekins <50034278+conekins@users.noreply.github.com>
2025-10-21 21:03:16 -0600

    finished displaying everything and added some basic styling

--- assignmentsFolder/09-peopleAssignment/js/scripts.js
+++ assignmentsFolder/09-peopleAssignment/js/scripts.js
@@ -10,20 +10,24 @@ const pathStart = "https://starwars.dgmuvu.com/characters/";
 function displayPeople(x) {
     x.forEach(person => {
         const myFigure = document.createElement('figure');
-        const myImg = document.createElement('img');
-        const myCaption = document.createElement('figcaption');
 
-        //add the src and the alt to the img
-        myImg.src = `${pathStart}${2}.jpg`;
+        const myImg = document.createElement('img');
+        const explodedArray = person.url.split('/');
+        console.log(explodedArray);
+
+        const charNumber = explodedArray[5];
+        myImg.src = `https://starwars.dgmuvu.com/characters/${charNumber}.jpg`;
         myImg.alt = `${person.name}`;
+
+        const myName = document.createElement('figcaption');
+        myName.textContent = person.name;
 
         // assemble the parts
         myFigure.appendChild(myImg);
-        myFigure.appendChild(myCaption);
+        myFigure.appendChild(myName);
 
         // attach to the HTML page
         myParent.appendChild(myFigure);
-
     }); // end of the loop
 };
 
