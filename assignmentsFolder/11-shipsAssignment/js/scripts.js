2268a6b83eb4df80d4c118557cac9b43b6b82991
Connor Ekins <50034278+conekins@users.noreply.github.com>
2025-11-03 22:35:19 -0700

    started setting up the assignment

--- assignmentsFolder/11-shipsAssignment/js/scripts.js
+++ assignmentsFolder/11-shipsAssignment/js/scripts.js
@@ -1,4 +1,14 @@
-import ships from `../data/starships.json`;
-
-pathStart = 'https://starwars.dgmuvu.com/ships/'
-imgError = 'https://starwars.dgmuvu.com/ships/placeholder.jpg'
+fetch('../data/starships.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Starships data:', data);
+    // You can now work with the JSON data here
+  })
+  .catch(error => {
+    console.error('Error fetching starships data:', error);
+  });
