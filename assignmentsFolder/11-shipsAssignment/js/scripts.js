664d70272f4ab9e50c05e109973d876e00ef78b7
Connor Ekins <50034278+conekins@users.noreply.github.com>
2025-11-11 13:19:08 -0700

    Added some of the JS

--- assignmentsFolder/11-shipsAssignment/js/scripts.js
+++ assignmentsFolder/11-shipsAssignment/js/scripts.js
@@ -1,14 +1,38 @@
-fetch('../data/starships.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Starships data:', data);
-    // You can now work with the JSON data here
-  })
-  .catch(error => {
-    console.error('Error fetching starships data:', error);
+const myNav = document.querySelector('nav');
+const myViewer = document.querySelector('main');
+
+const imgPath = 'https://starwars.dgmuvu.com/ships/';
+
+// grab the data and then wait for a response
+fetch("../data/starships.json")
+  .then((response) => response.json())
+  .then((shipArray) => {
+    console.log(shipArray);
+    populateNav(shipArray);
   });
+
+
+// populate the nav bar
+function populateNav(allShips) {
+  allShips.forEach(ship => {
+    let myButton = document.createElement('button');
+    myButton.textContent = ship.name
+    myButton.addEventListener('click', () => displayShip(ship));
+
+    myNav.appendChild(myButton);
+  });
+};
+
+function displayShip(shipData) {
+  let myFigure = document.createElement('figure')
+  let myImg = document.createElement('img')
+  let myCaption = document.createElement('figcaption')
+
+  myImg.src = '';
+  myCaption.textContent = '';
+
+  myFigure.appendChild(myImg);
+  myFigure.appendChild(myCaption);
+
+  myViewer.appendChild(myFigure);
+};
