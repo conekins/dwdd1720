20d5b21365aef5306f6156c8c6b116d30401cf63
Connor Ekins <50034278+conekins@users.noreply.github.com>
2025-11-11 23:16:05 -0700

    added more JS and some baseline CSS

--- assignmentsFolder/11-shipsAssignment/js/scripts.js
+++ assignmentsFolder/11-shipsAssignment/js/scripts.js
@@ -1,8 +1,6 @@
 const myNav = document.querySelector('nav');
 const myViewer = document.querySelector('main');
 
-const imgPath = 'https://starwars.dgmuvu.com/ships/';
-
 // grab the data and then wait for a response
 fetch("../data/starships.json")
   .then((response) => response.json())
@@ -11,25 +9,30 @@ fetch("../data/starships.json")
     populateNav(shipArray);
   });
 
-
 // populate the nav bar
 function populateNav(allShips) {
   allShips.forEach(ship => {
     let myButton = document.createElement('button');
-    myButton.textContent = ship.name
+    myButton.textContent = ship.name;
     myButton.addEventListener('click', () => displayShip(ship));
 
     myNav.appendChild(myButton);
   });
 };
 
-function displayShip(shipData) {
-  let myFigure = document.createElement('figure')
-  let myImg = document.createElement('img')
-  let myCaption = document.createElement('figcaption')
+function displayShip(ship) {
+  myViewer.textContent = '';
+  let myFigure = document.createElement('figure');
 
-  myImg.src = '';
-  myCaption.textContent = '';
+  let myImg = document.createElement('img');
+  const explodedArray = ship.url.split('/');
+  console.log(explodedArray);
+  const charNumber = explodedArray[5];
+  myImg.src = `https://starwars.dgmuvu.com/ships/${charNumber}.jpg`;
+  myImg.alt = ship.name;
+
+  let myCaption = document.createElement('figcaption');
+  myCaption.textContent = `${ship.name}`;
 
   myFigure.appendChild(myImg);
   myFigure.appendChild(myCaption);
