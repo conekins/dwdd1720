e0c1c9b277008d39a7566de214d136837cf44a55
Connor Ekins <50034278+conekins@users.noreply.github.com>
2025-12-12 03:28:55 -0700

    Completed up to Add-Navigation-Links

--- assignmentsFolder/13-storeAssignment/js/scripts.js
+++ assignmentsFolder/13-storeAssignment/js/scripts.js
@@ -1,1 +1,20 @@
-console.log("I am connected");
+import {friends} from "../data/friends.js";
+
+// grab the HTML references
+const myNav = document.querySelector('nav');
+const myViewer = document.querySelector('#viewer');
+
+// navigation
+friends.forEach(friend => {
+    console.log(friend)
+
+    const myBtn = document.createElement('button');
+    myBtn.textContent = `${friend.first} ${friend.last}`;
+    myBtn.addEventListener('click', () => showFriend(friend));
+
+    myNav.appendChild(myBtn);
+});
+
+function showFriend(x) {
+    console.log(x)
+};
